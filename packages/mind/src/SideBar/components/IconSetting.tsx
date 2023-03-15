92fad41e9857a258c1a7036dfaf7a0e19421521e
吕岳阳 <yueyang.lv@ucas.com.cn>
2023-03-15 09:27:12 +0800

    feat: 新增 置灰功能

--- packages/mind/src/SideBar/components/IconSetting.tsx
+++ packages/mind/src/SideBar/components/IconSetting.tsx
@@ -1,16 +1,20 @@
-import type { Node } from '@antv/x6';
+import type { Graph, Node } from '@antv/x6';
 import { SettingBarGroupItem } from '@kangmi/components';
+import { cls } from '@kangmi/utils';
 import { Col, Row } from 'antd';
+import { useEffect, useState } from 'react';
 import { useMindEditor } from '../../hooks';
 import { iconGroup } from '../../icons';
 import styles from '../index.module.less';
 
 export function IconSetting() {
   const { editor } = useMindEditor();
+  const { selectNodes } = useSelectedNodes(editor?.graph);
+
+  const disabled = !selectNodes?.length;
 
   const clickHandler = (groupName: string, iconName: string, isOnly: boolean) => {
-    const selectNodes = editor?.graph.getSelectedCells().filter((c) => c.isNode()) as Node[];
-    if (selectNodes?.length) {
+    if (!disabled) {
       selectNodes.forEach((node) => {
         editor?.addIcon(node, groupName, iconName, isOnly);
       });
@@ -23,12 +27,15 @@ export function IconSetting() {
         return (
           <div key={group.name}>
             <div className={styles.sectionTitle}>{group.cname}</div>
-            <Row className={styles.sectionContent} gutter={[0, 5]}>
+            <Row
+              className={cls([styles.sectionContent, disabled && styles.sectionContentDisabled])}
+              gutter={[0, 5]}
+            >
               {group.icons.map((item) => {
                 return (
                   <Col xs={3} key={item.name}>
                     <span
-                      style={{ fontSize: 24, cursor: 'pointer' }}
+                      className={styles.iconItem}
                       onClick={() => {
                         clickHandler(group.name, item.name, !!group.isOnly);
                       }}
@@ -44,4 +51,34 @@ export function IconSetting() {
       })}
     </SettingBarGroupItem>
   );
+}
+
+function useSelectedNodes(graph?: Graph) {
+  const [selectNodes, setSelectNodes] = useState<Node[]>();
+
+  useEffect(() => {
+    if (!graph) return;
+    const selectedHandler = () => {
+      const cells = graph.getSelectedCells();
+      setSelectNodes(cells.filter((cell): cell is Node => cell.isNode()));
+    };
+    selectedHandler();
+    let timer: NodeJS.Timeout;
+    const selectFn = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        selectedHandler();
+      });
+    };
+
+    graph.on('selection:changed', selectFn);
+
+    return () => {
+      graph.off('selection:changed', selectFn);
+    };
+  }, [graph]);
+
+  return {
+    selectNodes,
+  };
 }
