aad947a9c6a367aab3fe654a90b8867223ccb6b6
吕岳阳 <yueyang.lv@ucas.com.cn>
2023-03-17 16:35:20 +0800

    feat: 右键菜单新增添加主题功能

--- packages/mind/src/Editor/contextmenu/index.tsx
+++ packages/mind/src/Editor/contextmenu/index.tsx
@@ -4,6 +4,7 @@ import type { Node } from '@antv/x6';
 import { ContextMenu } from '@kangmi/components';
 import { message } from 'antd';
 import type { Editor } from '../index';
+import { RootNodeConfig } from '../nodes';
 
 let contextMenu: ContextMenu | undefined;
 
@@ -12,7 +13,7 @@ type DeepCloneNode = Node & { _children?: DeepCloneNode[] };
 /** 右键菜单 */
 export function registerContextMenu(editor: Editor) {
   const graph = editor.graph;
-  graph.on('cell:contextmenu', ({ e, cell, x, y }) => {
+  graph.on('node:contextmenu', ({ e, cell }) => {
     e.stopPropagation();
 
     const rootContainer = graph.container.parentElement!;
@@ -31,13 +32,33 @@ export function registerContextMenu(editor: Editor) {
     }
 
     const position = { x: e.pageX + left - offsetLeft, y: e.pageY + top - offsetTop };
-    const selectedCells = graph.getSelectedCells();
-    const selectedNodes = selectedCells.filter((c) => c.isNode()) as Node[];
+    const { selectedCells, firstSelectedNode } = editor.selectedHelper();
     const clipboardCells = graph.getCellsInClipboard();
     const clipboardNodes = clipboardCells.filter((c): c is DeepCloneNode => c.isNode());
     contextMenu.show({
       position,
       items: [
+        {
+          label: '新增子主题',
+          key: 'appendChild',
+          extra: 'Tab',
+          onClick: () => {
+            editor.appendChildNode();
+          },
+        },
+        {
+          label: '新增同级主题',
+          key: 'appendNeighbor',
+          extra: 'Tab',
+          disabled: firstSelectedNode.shape === RootNodeConfig.NODE_NAME,
+          onClick: () => {
+            editor.appendNeighborNode();
+          },
+        },
+        {
+          type: 'divider',
+          key: 'd0',
+        },
         {
           label: '复制',
           key: 'copy',
